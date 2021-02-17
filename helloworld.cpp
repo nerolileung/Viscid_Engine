46602b16a3bfea46853f1f48dc25bf209ceb7424
Nero Leung <nerolileung@gmail.com>
2021-02-17 18:07:13 +0000

    build pipeline works
    requires visual studio code, clang, cmake

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "ViscidConfig.h"
+
+#ifdef USE_MYMATH
+#  include "MathFunctions.h"
+#endif
+
+int main()
+{
+    std::cout << "Hello C++ World from VS Code and the C++ extension! \n";
+}
