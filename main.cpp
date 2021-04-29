5d69dd2732afe75814c3e377abdace972a04ac84
Nero Leung <nerolileung@gmail.com>
2021-04-29 11:45:11 +0100

    barebones update/render loop

--- main.cpp
+++ main.cpp
@@ -1,25 +1,38 @@
 #include "ViscidConfig.h"
 #include "SDL2/SDL.h"
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-  SDL_Init(SDL_INIT_VIDEO);
+  // initialise sdl for graphics
+  if (SDL_Init(SDL_INIT_VIDEO) < 0){
+    std::cout << "Failed to initialise video!";
+    exit(-1);
+  }
 
-  SDL_Window *window = SDL_CreateWindow(
-    "SDL2Test",
-    SDL_WINDOWPOS_UNDEFINED,
-    SDL_WINDOWPOS_UNDEFINED,
-    640,
-    480,
-    0
-  );
+  // create 640x480 window
+  SDL_Window *window = SDL_CreateWindow("SDL2Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+  // create renderer with window context using first rendering driver with hardware acceleration
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  if (!window){
+    std::cout << "Failed to create window!";
+    exit(-1);
+  }
 
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
+  // put a null event in the queue
+  SDL_Event event;
+  // game loop
+  while (SDL_PollEvent(&event) >= 0){
+    // update loop; check if escape has been pressed
+    const Uint8* keystate = SDL_GetKeyboardState(NULL);
+    if (keystate[SDL_SCANCODE_ESCAPE])
+      break;
 
-  SDL_Delay(3000);
+    // render loop; clear to black
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+  }
 
   SDL_DestroyWindow(window);
   SDL_Quit();
